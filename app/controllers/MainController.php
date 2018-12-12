c79a896086dda3590e89a55c9382c6869bb56df5
ing <inyoman_gurnitha@yahoo.com>
2018-12-12 11:05:59 +0700

    Find and Display Brands

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -7,7 +7,12 @@ use ishop\Cache;
 class MainController extends AppController {
 
     public function indexAction(){
+        
         $this->setMeta('Home Page', 'Description...', 'Keyword...');
+
+        $brands = \R::find('brand');
+        // debug($brands);
+        $this->set(compact('brands', 'hits'));
     }
 
 }
--- app/views/Main/index.php
+++ app/views/Main/index.php
@@ -40,19 +40,22 @@
 			  </script>
 			<!--End-slider-script-->
 	<!--about-starts-->
+	<?php if($brands) : ?>
 	<div class="about"> 
 		<div class="container">
 			<div class="about-top grid-1">
-				<div class="col-md-4 about-left">
-					<figure class="effect-bubba">
-						<img class="img-responsive" src="images/abt-1.jpg" alt=""/>
-						<figcaption>
-							<h2>Nulla maximus nunc</h2>
-							<p>In sit amet sapien eros Integer dolore magna aliqua</p>	
-						</figcaption>			
-					</figure>
-				</div>
-				<div class="col-md-4 about-left">
+				<?php foreach ($brands as $brand): ?>
+					<div class="col-md-4 about-left">
+						<figure class="effect-bubba">
+							<img class="img-responsive" src="images/<?=$brand->img; ?>" alt=""/>
+							<figcaption>
+								<h2><?=$brand->title; ?></h2>
+								<p><?=$brand->description; ?></p>	
+							</figcaption>			
+						</figure>
+					</div>
+				<?php endforeach; ?>
+				<!-- <div class="col-md-4 about-left">
 					<figure class="effect-bubba">
 						<img class="img-responsive" src="images/abt-2.jpg" alt=""/>
 						<figcaption>
@@ -69,11 +72,12 @@
 							<p>In sit amet sapien eros Integer dolore magna aliqua</p>	
 						</figcaption>			
 					</figure>
-				</div>
+				</div> -->
 				<div class="clearfix"></div>
 			</div>
 		</div>
 	</div>
+	<?php endif; ?>
 	<!--about-end-->
 	<!--product-starts-->
 	<div class="product"> 
